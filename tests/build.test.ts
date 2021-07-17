4ade266a7848b4a70ac10297d4a93789c6bed24a
Ryan <ryanpivovar@gmail.com>
2021-07-16 20:17:16 -0400

    update formatting in test

--- tests/build.test.ts
+++ tests/build.test.ts
@@ -1,12 +1,9 @@
 import { lint } from "../build.ts";
 import { assertEquals } from "https://deno.land/std@0.100.0/testing/asserts.ts";
 
-Deno.test({
-  name: "lint(): run successfully",
-  async fn(): Promise<void> {
-    Deno.writeTextFileSync("temp.ts", "console.log();");
-    const status = await lint();
-    assertEquals(status.success, true);
-    Deno.removeSync("temp.ts");
-  },
+Deno.test("lint(): run successfully", async (): Promise<void> => {
+  Deno.writeTextFileSync("temp.ts", "console.log();");
+  const status = await lint();
+  assertEquals(status.success, true);
+  Deno.removeSync("temp.ts");
 });
