9202920c783aa5322cf022dd5cf826f063cebee7
Ryan <ryanpivovar@gmail.com>
2021-07-17 15:50:50 -0400

    add test cases for runCommand

---
+++ util/tests/runCommand.test.ts
@@ -0,0 +1,21 @@
+Deno.test(
+  "call the log function if the Deno.run() process produces a stdout with length",
+  () => {}
+);
+
+Deno.test(
+  "call the log function if the Deno.run() process produces a stderr with length",
+  () => {}
+);
+
+Deno.test(
+  "return a status object with a success key that is false when an error occurs during the Deno.run() process",
+  () => {}
+);
+
+Deno.test(
+  "return a status object with a success key that is true when the Deno.run() process finishes without error",
+  () => {}
+);
+
+Deno.test("close all Deno processes that were opened during its run", () => {});
